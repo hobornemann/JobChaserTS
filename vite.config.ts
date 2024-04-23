b38937ca625df992d772744623d5ce9d958eb4d8
hobornemann <hobornemann@gmail.com>
2024-04-23 10:08:30 +0200

    changed to   base: "/jobchaser-ts" in vite.config.ts (deleted last fwd slash)

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/jobchaser-ts/",
+  base: "/jobchaser-ts",
   plugins: [react()],
 })
